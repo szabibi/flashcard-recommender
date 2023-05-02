0f217f5703192a9f80698c24a16106ce24417575
szabibi <bagszabolcs@gmail.com>
2023-05-02 17:10:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+tk.Label(text='My first Graphical Interface',
+         foreground='black',
+         background='chartreuse').pack()
+
+window.mainloop()
