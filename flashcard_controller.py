116974b8e70ebbe33d61dbafe137e87aa2f30d96
szabibi <bagszabolcs@gmail.com>
2023-05-10 12:31:10 +0200

    Marking answer right and wrong working

--- flashcard_controller.py
+++ flashcard_controller.py
@@ -3,6 +3,23 @@ import db
 
 BTN_REVEAL_TXT = ('Show', 'Next')
 
+def show_hide_buttons(btn_show, btn_right, btn_wrong):
+    if not answer_revealed:
+        btn_show.grid(column=0, row=0, pady=(10, 5))
+        btn_right.grid_forget()
+        btn_wrong.grid_forget()
+    else:
+        btn_show.grid_forget()
+        btn_right.grid(column=0, row=0, padx=5, pady=(10, 5))
+        btn_wrong.grid(column=1, row=0, padx=5, pady=(10, 5))
+
+def set_button_state(btns, state):
+    for btn in btns:
+        btn.config(state=state)
+
+def update_slider(val, lbl):
+    lbl.config(text='%0.2f' % float(val))
+
 def init_card_sequence():
     global word_sequence, current_card_number
     word_sequence = [i for i in range(word_count)]
@@ -10,8 +27,9 @@ def init_card_sequence():
     current_card_number = 1
     return word_sequence.pop(0)
 
-def load_new_set(lbl_card=None, lbl_page=None, lbl_max_page=None):
-    global words, word_count, word_idx, answer_revealed
+def load_new_set(btns=None, lbl_card=None, lbl_page=None, lbl_max_page=None):
+    global words, word_count, word_idx, answer_revealed, right_answer_count
+
     success = False
     while not success:
         try:
@@ -20,15 +38,20 @@ def load_new_set(lbl_card=None, lbl_page=None, lbl_max_page=None):
             words = db.fetch_flaschards(set)
 
             word_count = len(words)
+            right_answer_count = 0
             word_idx = init_card_sequence()
+
+            answer_revealed = False
 
             if lbl_page:
                 lbl_page.config(text='1')
             if lbl_card and lbl_page and lbl_max_page:
                 update_card_label(lbl_card, lbl_page, lbl_max_page)
 
-            answer_revealed = False
             success = True
+
+            if btns:
+                set_button_state(btns, 'normal')
         except IndexError:
             pass
 
@@ -56,25 +79,35 @@ def update_card_label(lbl_card, lbl_page, lbl_max_page=None):
     if lbl_max_page:
         lbl_max_page.config(text=f'/ {word_count}')
 
-def load_next_card(btn, lbl_card, lbl_page):
-    global word_idx
-    global current_card_number
-    global answer_revealed
-    global word_sequence
+def load_next_card(btn_show, btn_right, btn_wrong, btn_shuffle, lbl_card, lbl_page, inc_score=False):
+    global word_idx,current_card_number, answer_revealed,word_sequence, right_answer_count
+
+    print('ANSWER:', answer_revealed)
+
+    if inc_score:
+        right_answer_count += 1
+        print(right_answer_count)
 
     if answer_revealed:
-        word_idx = get_next_word()
-        btn_txt = BTN_REVEAL_TXT[0]
-    else:
-        btn_txt = BTN_REVEAL_TXT[1]
+        if len(word_sequence) == 0: # elfogytak a szavak
 
-    btn.config(text=btn_txt)
+            lbl_card.config(text=f"All cards reviewed.\nYou got {right_answer_count} right,\nand {word_count-right_answer_count} wrong.\n\nStats recorded.")
+            set_button_state((btn_show, btn_right, btn_wrong, btn_shuffle), 'disabled')
+
+            return
+        else:
+            current_card_number += 1
+            word_idx = word_sequence.pop(0)
+
     answer_revealed = not answer_revealed
+
+    show_hide_buttons(btn_show, btn_right, btn_wrong)
 
     update_card_label(lbl_card, lbl_page)
 
-def shuffle_set(btn, lbl_card, lbl_page):
+def shuffle_set(btn_show, btn_right, btn_wrong, lbl_card, lbl_page):
     global word_sequence, word_idx, answer_revealed, current_card_number
+
     if answer_revealed:
         # ha már fel lett fedve a kihúzott kártya, tovább is lép egyszerre
         current_card_number += 1
@@ -82,6 +115,8 @@ def shuffle_set(btn, lbl_card, lbl_page):
     else:
         # ha még nem lett felfedve a válasz, visszarakja a kártyák közé
         word_sequence.append(word_idx)
+
+    show_hide_buttons(btn_show, btn_right, btn_wrong)
 
     random.shuffle(word_sequence)
     word_idx = word_sequence.pop(0)
--- gui.py
+++ gui.py
@@ -13,10 +13,10 @@ class GUI:
     def __init__(self, parent):
         print('Start of GUI')
 
-        frm_buttons = tk.Frame(width=100, bg='black')
+        frm_buttons = tk.Frame(width=100)
         frm_buttons.grid(column=0, row=0, sticky='nw')
 
-        frm_card = tk.Frame(bg='lime', width=600, height=400)
+        frm_card = tk.Frame(width=600, height=400)
         frm_card.grid(column=0, row=1, sticky='we')
 
         lbl_card_txt = tk.Label(text=words[word_idx][0],
@@ -26,26 +26,40 @@ class GUI:
                                 master=frm_card)
         lbl_card_txt.pack()
 
-        frm_card_buttons = tk.Frame(width=300, height=50, master=frm_card)
-        frm_card_buttons.pack()
+        frm_card_buttons_top = tk.Frame(width=300, height=20)
+        frm_card_buttons_top.grid(column=0, row=2)
+
+        frm_card_buttons_bottom = tk.Frame(width=300, height=20)
+        frm_card_buttons_bottom.grid(column=0, row=3)
 
         btn_reveal = ttk.Button(text=BTN_REVEAL_TXT[0],
-                                master=frm_card_buttons,
-                                command=lambda: load_next_card(btn_reveal, lbl_card_txt, lbl_current_page))
+                                master=frm_card_buttons_top,
+                                command=lambda: load_next_card(btn_reveal, btn_mark_right, btn_mark_wrong, btn_shuffle, lbl_card_txt, lbl_current_page))
 
-        btn_reveal.pack(side=tk.LEFT, padx=10, pady=10)
+        btn_reveal.grid(column=0, row=0, pady=(10,  5))
+
+        btn_mark_right = ttk.Button(text='Right',
+                                    master=frm_card_buttons_top,
+                                    command=lambda: load_next_card(btn_reveal, btn_mark_right, btn_mark_wrong, btn_shuffle, lbl_card_txt, lbl_current_page, inc_score=True))
+        btn_mark_wrong = ttk.Button(text='Wrong',
+                                    master=frm_card_buttons_top,
+                                    command=lambda: load_next_card(btn_reveal, btn_mark_right, btn_mark_wrong, btn_shuffle, lbl_card_txt, lbl_current_page))
+
+        #btn_mark_correct.grid(column=0, row=0, padx=5, pady=(10,  5))
+        #btn_mark_wrong.grid(column=1, row=0, padx=5, pady=(10, 5))
 
         btn_shuffle = ttk.Button(text='Shuffle',
-                                 master=frm_card_buttons,
-                                 command=lambda: shuffle_set(btn_shuffle, lbl_card_txt, lbl_current_page))
+                                 master=frm_card_buttons_bottom,
+                                 command=lambda: shuffle_set(btn_reveal, btn_mark_right, btn_mark_wrong, lbl_card_txt, lbl_current_page))
 
-        btn_shuffle.pack(side=tk.RIGHT)
+        btn_shuffle.grid(column=0, row=1)
 
-        frm_stats = tk.Frame(bg='chartreuse')
+        frm_stats = tk.Frame()
 
         frm_stats.grid(column=0,
-                       row=2,
-                       sticky='wn')
+                       row=4,
+                       sticky='n',
+                       pady=5)
 
         lbl_current_page_txt = tk.Label(text='Card:',
                                         font=FONT_SMALL,
@@ -68,16 +82,48 @@ class GUI:
         lbl_max_page.grid(column=3,
                           row=0)
 
-        btn_new_set_save = ttk.Button(text='Save & New set', master=frm_buttons)
-        btn_new_set_save.pack(side=tk.LEFT, padx=10, pady=10)
+        # New Set buttons
+        # btn_new_set_save = ttk.Button(text='Save & New set', master=frm_buttons)
+        # btn_new_set_save.grid(column=0, row=0, padx=(10,0))
 
         btn_new_set_no_save = ttk.Button(text='New set',
                                          master=frm_buttons,
-                                         command=lambda: load_new_set(lbl_card_txt,
+                                         command=lambda: load_new_set((btn_reveal, btn_mark_right, btn_mark_wrong, btn_shuffle),
+                                                                      lbl_card_txt,
                                                                       lbl_current_page,
                                                                       lbl_max_page))
-        btn_new_set_no_save.pack(side=tk.RIGHT)
+        btn_new_set_no_save.grid(column=0, row=0, padx=(10,0), pady=10)
+
+        lbl_new_set_warning = tk.Label(text='(Current progress will be lost!)',
+                            master=frm_buttons)
+
+        lbl_new_set_warning.grid(column=1, row=0, pady=10)
+
+        # Priority sliders
+        i = 1
+        sliders = {}
+        for txt in ("Last reviewed", "Least reviewed", "Least accurate"):
+            lbl_txt = tk.Label(text=txt,
+                               master=frm_buttons)
+            lbl_txt.grid(column=0, row = i, padx=(10,0), pady=(0,5), sticky='w')
+
+            lbl_val = tk.Label(text="0.0", master=frm_buttons)
+            lbl_val.grid(column=2, row=i, pady=(0,5), padx=10, sticky='w')
+
+            sld = ttk.Scale(from_=0, to=5,
+                            master=frm_buttons,
+                            command=lambda value, lbl=lbl_val: update_slider(value, lbl))
+            sld.grid(column=1, row=i, pady=(0,5))
+
+            sliders[txt]=sld
+            i += 1
+
+        print(sliders)
+
+
 
 window = tk.Tk()
 window.columnconfigure(0, weight=1, minsize=200)
 window.rowconfigure([0, 2], weight=1, minsize=50)
+window.resizable(width=False, height=False)
+window.title('Flashcards')
