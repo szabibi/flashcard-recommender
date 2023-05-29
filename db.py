6b48993f4ca7c1ff1f433e2ba12493e38f360f6c
szabibi <bagszabolcs@gmail.com>
2023-05-29 14:25:53 +0200

    started work on deck editing

--- db.py
+++ db.py
@@ -18,8 +18,16 @@ def get_largest_count():
 def get_set_name(set_id):
     return cur.execute(f'SELECT name FROM sets where id = {set_id}').fetchone()[0]
 
+def get_set_id(set_name):
+    return cur.execute(f'SELECT id FROM sets where name = {set_name}').fetchone()[0]
+
+def fetch_all_sets():
+    res = cur.execute('SELECT id, name FROM sets').fetchall()
+    #return [i[0] for i in res]
+    return res
+
 def fetch_flaschards(set_id):
-    res = cur.execute(f'SELECT front, back FROM cards WHERE set_id = {set_id}')
+    res = cur.execute(f'SELECT front, back, hint FROM cards WHERE set_id = {set_id}')
     return res.fetchall()
 
 def fetch_set_stats(set_id):
@@ -29,6 +37,9 @@ def fetch_set_stats(set_id):
 def fetch_all_set_stats():
     res = cur.execute(f'SELECT id, last_reviewed, count, accuracy FROM sets WHERE last_flag=0')
     return res.fetchall()
+
+def get_deck_size(set_id):
+    return cur.execute(f'SELECT COUNT(*) FROM cards WHERE set_id = {set_id}').fetchone()[0]
 
 def register_review_stats(set_id, accuracy):
     cur.execute(f'UPDATE sets SET last_flag=0 WHERE last_flag=1')
--- gui.py
+++ gui.py
@@ -10,13 +10,16 @@ FONT_BIG = (FONT_NAME, 16)
 FONT_SMALL = (FONT_NAME, 10)
 
 class GUI:
+    edit_decks_window_is_open = False
+    edit_decks_window = None
+
     def __init__(self, parent):
         print('Start of GUI')
 
         frm_buttons = tk.Frame(width=100)
         frm_buttons.grid(column=0, row=0, sticky='nw')
 
-        frm_card = tk.Frame(width=600, height=400, borderwidth=1, relief="solid")
+        frm_card = tk.Frame(width=600, height=400, borderwidth=1, relief='solid')
         frm_card.grid(column=0, row=3, sticky='we', padx=20)
 
         lbl_card_txt = tk.Label(text='',
@@ -63,7 +66,7 @@ class GUI:
                        sticky='n',
                        pady=5)
 
-        lbl_set_name=tk.Label(text='Click "New set" to start!',
+        lbl_set_name=tk.Label(text='Click "New deck" to start!',
                               font=FONT_BIG,
                               width=30
                               )
@@ -95,7 +98,7 @@ class GUI:
         # btn_new_set_save = ttk.Button(text='Save & New set', master=frm_buttons)
         # btn_new_set_save.grid(column=0, row=0, padx=(10,0))
 
-        btn_new_set_no_save = ttk.Button(text='New set',
+        btn_new_set_no_save = ttk.Button(text='New deck',
                                          master=frm_buttons,
                                          command=lambda: load_new_set((btn_reveal, btn_mark_right, btn_mark_wrong, btn_shuffle),
                                                                       lbl_card_txt,
@@ -109,6 +112,12 @@ class GUI:
                             master=frm_buttons)
 
         lbl_new_set_warning.grid(column=1, row=0, pady=10)
+
+        btn_edit_decks = ttk.Button(text='Edit decks',
+                                    master=frm_buttons,
+                                    command=self.open_edit_decks_window)
+        btn_edit_decks.grid(column=2, row=0,
+                            padx=(30, 0))
 
         # Priority sliders
         i = 1
@@ -131,10 +140,171 @@ class GUI:
 
         print(sliders)
 
+    def open_edit_decks_window(self):
+        if not self.edit_decks_window_is_open:
+            self.edit_decks_window_is_open = True
+            self.edit_decks_window = EditDeckGUI(window)
+            self.edit_decks_window.protocol('WM_DELETE_WINDOW', lambda: self.close_edit_decks_window(self.edit_decks_window))
 
+        self.edit_decks_window.lift()
+
+    def close_edit_decks_window(self, window):
+        print("Closed")
+        self.edit_decks_window_is_open = False
+        window.destroy()
+
+class EditDeckGUI(tk.Toplevel):
+    def __init__(self, master=None):
+
+        super().__init__(master=master)
+
+        # Frame of top menu
+        frm_top_menu = tk.Frame(width=1000, master=self)
+        frm_top_menu.grid(row=0, column=0)
+
+        # Frame of deck title
+        frm_deck_name = tk.Frame(master=self, width=1000)
+        frm_deck_name.grid(row=1, column=0)
+
+        # Frame of cards in deck
+        self.frm_canvas = tk.Frame(master=self, width=800, height=100)
+        self.frm_canvas.grid(row=2, column=0, sticky='news')
+        self.frm_canvas.grid_rowconfigure(0, weight=1)
+        self.frm_canvas.grid_columnconfigure(0, weight=1)
+
+        self.canvas = tk.Canvas(master=self.frm_canvas, width=470, height=400)
+        self.canvas.grid(row=0, column=0, sticky='news')
+
+        # Label for drop-down menu
+        tk.Label(text='Deck to edit: ', master=frm_top_menu).grid(column=0, row=0)
+
+        # Drop-down menu
+        self.selected_deck = tk.StringVar(self)
+        decks = db.fetch_all_sets()
+        deck_names = sorted([decks[i][1] for i in range(len(decks))])
+        self.decks_dict = { decks[i][1]:decks[i][0] for i in range(len(decks))}
+
+        lst_decks = ttk.OptionMenu(frm_top_menu, self.selected_deck, deck_names[0], *deck_names)
+        lst_decks.grid(column=1, row=0)
+
+        # Load deck button
+        ttk.Button(text='Open',
+                   master=frm_top_menu,
+                   command=self.load_deck_to_edit)\
+            .grid(column=2, row=0)
+
+        # Deck title
+        self.lbl_selected_deck = tk.Label(font=FONT_BIG,
+                                          text=self.selected_deck.get(),
+                                          master=frm_deck_name)
+        self.lbl_selected_deck.grid(column=0, row=1, sticky='w')
+
+    def load_deck_to_edit(self, *args):
+        global frm_deck_cards
+
+        self.lbl_selected_deck.config(text=self.selected_deck.get())
+        deck_id = self.decks_dict[self.selected_deck.get()]
+
+        self.canvas.delete('all')
+
+        frm_inside_canvas = tk.Frame(master=self.canvas)
+        self.canvas.create_window((0, 0), window=frm_inside_canvas, anchor='nw')
+
+        frm_deck_cards = tk.Frame(master=frm_inside_canvas)
+        frm_deck_cards.grid(row=0, column=0)
+
+        frm_deck_buttons = tk.Frame(master=frm_inside_canvas)
+        frm_deck_buttons.grid(row=1, column=0, pady=20)
+
+        btn_add_new_card = ttk.Button(text='Add', master=frm_deck_buttons, command=self.create_new_card_fields)
+        btn_add_new_card.pack()
+
+        # Scrollbar
+        vbar = ttk.Scrollbar(master=self.frm_canvas, orient='vertical', command=self.canvas.yview)
+        vbar.grid(row=0, column=1, sticky='ns')
+        self.canvas.configure(yscrollcommand=vbar.set)
+
+        # hbar = ttk.Scrollbar(master=frm_canvas, orient='horizontal', command=canvas.xview)
+        # hbar.grid(row=1, column=0, sticky='ew')
+        # canvas.configure(yscrollcommand=hbar.set)
+
+        # Cards
+        # deck_size
+        deck_cards = db.fetch_flaschards(deck_id)
+        print(deck_cards)
+        self.deck_size = len(deck_cards)
+
+        self.entry_field_frames = []
+        self.entry_field_labels = []
+        for i in range(self.deck_size):
+            self.create_card_fields(frm_deck_cards, i, deck_cards[i][0], deck_cards[i][1], deck_cards[i][2])
+
+            # txt_front_side = tk.Entry(master=frm_deck_cards)
+            # txt_front_side.grid(row=i, column=0, pady=10)
+
+        # canvas.config(scrollregion=canvas.bbox('all'))
+        self.canvas.config(scrollregion=(0, 0, 450, 51 * self.deck_size + 50))
+
+    def create_new_card_fields(self):
+        global frm_deck_cards
+
+        self.deck_size += 1
+        print(self.deck_size)
+
+        self.create_card_fields(frm_deck_cards, self.deck_size-1)
+        self.canvas.config(scrollregion=(0, 0, 450, 51 * self.deck_size + 50))
+        self.canvas.yview_moveto(1)
+
+    def delete_card(self, frm):
+        frm.grid_remove()
+
+        idx = self.entry_field_frames.index(frm)
+
+        for i in range(idx+1, len(self.entry_field_frames)):
+            new_id = int(self.entry_field_labels[i]["text"])-1
+            self.entry_field_labels[i].config(text=new_id)
+            self.entry_field_frames[i].grid_forget()
+            self.entry_field_frames[i].grid(row=new_id-1, column=0)
+
+        del self.entry_field_frames[idx]
+        del self.entry_field_labels[idx]
+        self.deck_size -= 1
+        self.canvas.config(scrollregion=(0, 0, 450, 51 * self.deck_size + 50))
+
+    def create_card_fields(self, master, row, front='', back='', hint=''):
+        frm_entry = tk.Frame(master=master)
+        frm_entry.grid(row=row, column=0)
+
+        lbl_card_number = tk.Label(master=frm_entry, text=f'{row + 1}')
+        lbl_card_number.grid(row=row, column=0, sticky='nsw', padx=10, pady=(10 if row > 0 else 0, 0))
+
+        tk.Label(master=frm_entry, text='Front side').grid(row=row, column=1, sticky='nw',
+                                                                pady=(10 if row > 0 else 0, 20))
+        entry_front_side = tk.Entry(master=frm_entry)
+        entry_front_side.grid(row=row, column=1, sticky='sw', padx=(0, 10))
+        entry_front_side.insert(0, front)
+
+        tk.Label(master=frm_entry, text='Back side').grid(row=row, column=2, sticky='nw',
+                                                               pady=(10 if row > 0 else 0, 20))
+        entry_back_side = tk.Entry(master=frm_entry)
+        entry_back_side.grid(row=row, column=2, sticky='sw', padx=(0, 10))
+        entry_back_side.insert(0, back)
+
+        tk.Label(master=frm_entry, text='Hint side').grid(row=row, column=3, sticky='nw',
+                                                               pady=(10 if row > 0 else 0, 20))
+        entry_hint_side = tk.Entry(master=frm_entry)
+        entry_hint_side.grid(row=row, column=3, sticky='sw')
+        entry_hint_side.insert(0, hint)
+
+        btn_delete = ttk.Button(text='X', master=frm_entry, width=3,
+                                command=lambda:self.delete_card(frm_entry))
+        btn_delete.grid(row=row, column=4, sticky='e', padx=10)
+
+        self.entry_field_frames.insert(row, frm_entry)
+        self.entry_field_labels.insert(row, lbl_card_number)
 
 window = tk.Tk()
-window.columnconfigure(0, weight=1, minsize=200)
+window.columnconfigure(0, weight=1, minsize=400)
 window.rowconfigure([0, 2], weight=1, minsize=50)
 window.resizable(width=False, height=False)
 window.title('Flashcards')
